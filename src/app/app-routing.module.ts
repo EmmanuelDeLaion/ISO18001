81e57935f476cf243f308ecb2dc6a44b12999ad4
Emmanuel De Leon <deleon.lerma.17163@itsmante.edu.mx>
2020-10-27 00:52:12 -0600

    imagenes y home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 const routes: Routes = [
- {}
+ {
+   path: 'home',component: LandingPageComponent,
+ }
+
 ];
 
 @NgModule({
